ec76f9e2784f6914961ead690c6f63d6df823c19
Anderson Vitor <andersonvittor1801@gmail.com>
2024-05-16 09:36:58 -0300

    style(cart and cartitem): styling both components

--- src/Pages/Cart/Styles.jsx
+++ src/Pages/Cart/Styles.jsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
 export const cart = styled.div`
-
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `
 export const h1 = styled.h1``
 
@@ -10,9 +13,42 @@ export const cartItems = styled.div``
 // CartItem
 
 export const cartItem = styled.div`
+    width: 700px;
+    height: 250px;
+    display: flex;
 
+    align-items: center;
+    box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
+    border-radius: 25px;
+    margin: 30px;
+
+    img {
+        width: 200px;
+    }
 `
 
 export const description = styled.div`
+    width: 100%;
+    font-size: 30px;
+`
 
+export const countHandler = styled.div`
+    input {
+        width: 40px;
+        text-align: center;
+        font-weight: bolder; 
+    }
+`
+
+export const checkout = styled.div`
+    button {
+        width: 150px;
+        height: 56px;
+        background-color: rgb(19, 19, 19);
+        color: white;
+        border: none;
+        border-radius: 8px;
+        margin: 10px;
+        cursor: pointer;
+    }
 `
